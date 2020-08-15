5fb44177c0171024dfd1f5f3de13e5d9c044090f
edwingl10 <40483569+edwingl10@users.noreply.github.com>
2020-08-15 16:42:44 -0700

    added cookies
    - use passport to add cookie sessions
    - remembers when user log in
    - log out functionality complete

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+//const md5 = require("md5");
+//const bcrypt = require("bcrypt");
+//const saltRounds = 10; 
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 //const encrypt = require("mongoose-encryption");
 
 const app = express();
@@ -16,18 +19,33 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
+app.use(session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false
+}));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.set('useCreateIndex', true);
 
 const userSchema = new mongoose.Schema({
     email: String, 
     password: String
 });
 
+//used to hash and salt passwords
+userSchema.plugin(passportLocalMongoose);
 
 //userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
 
-const User = new mongoose.model("USer", userSchema);
+const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res){
     res.render("home");
@@ -41,6 +59,7 @@ app.get("/register", function(req, res){
     res.render("register");
 });
 
+/*
 app.post("/register", function(req ,res){
     bcrypt.hash(req.body.password, saltRounds, function(err, hash){
         const newUser = new User({
@@ -76,7 +95,55 @@ app.post("/login", function(req,res){
             }
         }
     });
+});*/
+
+
+//uses cookie to check if already logged in, can skip login/register page
+app.get("/secrets", function(req, res){
+    if(req.isAuthenticated()){
+        res.render("secrets");
+    }else{
+        res.redirect("/login");
+    }
 });
+
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/");
+});
+
+app.post("/register", function(req, res){
+    User.register({username: req.body.username}, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            res.redirect("/register");
+        }else{
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/secrets");
+            });
+        }
+    });
+});
+
+
+app.post("/login", function(req, res){
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    req.login(user, function(err){
+        if(err){
+            console.log(err);
+        }else{
+            passport.authenticate("local")(req,res, function(){
+                res.redirect("/secrets");
+            });
+        }
+    });
+
+});
+
 
 app.listen(3000, function(){
     console.log("Server started on port 3000.");
